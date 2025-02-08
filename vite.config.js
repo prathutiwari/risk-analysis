5331325bf651f068961fc078e360d042164a4282
prathutiwari <prathutiwari22@gmail.com>
2025-02-08 17:40:45 +0530

    added header file

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/', // Ensure base is set correctly
+  base: './', // Ensure base is set correctly
 });
